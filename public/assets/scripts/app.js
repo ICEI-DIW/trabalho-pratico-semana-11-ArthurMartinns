cae2ede5cdb771b1fb8c29bccee2d71962438fd2
ArthurMartinns <arthurmartiinscontato@gmail.com>
2025-05-08 08:52:04 -0300

    feat: initial structure

---
+++ public/assets/scripts/app.js
@@ -0,0 +1,297 @@
+const dados = {
+    "filmes": [
+        {
+            "id": 1,
+            "titulo": "Ilha do Medo",
+            "descricao": "Um agente federal investiga o desaparecimento de uma paciente em um hospital psiquiátrico isolado.",
+            "sinopse": "Em 1954, os agentes federais Teddy Daniels e Chuck Aule são enviados ao Hospital Ashecliffe, localizado na remota Shutter Island, para investigar o desaparecimento de uma paciente. À medida que a investigação avança, Teddy descobre segredos obscuros que o fazem questionar sua própria sanidade.",
+            "genero": "Suspense / Mistério",
+            "diretor": "Martin Scorsese",
+            "destaque": true,
+            "data_lancamento": "2010-03-12",
+            "imagem_principal": "assets/assets/shutterIsland.webp",
+            "imagens_complementares": [
+                {
+                    "id": 1,
+                    "src": "assets/assets/ilha_medo01.webp",
+                },
+                {
+                    "id": 2,
+                    "src": "assets/assets/ilha_medo02.webp",
+                },
+                {
+                    "id": 3,
+                    "src": "assets/assets/ilha_medo03.webp",
+                },
+                {
+                    "id": 4,
+                    "src": "assets/assets/ilha_medo04.webp",
+                },
+            ]
+        },
+        {
+            "id": 2,
+            "titulo": "O Preço da Verdade",
+            "descricao": "Um advogado enfrenta uma poderosa corporação química em uma batalha judicial que expõe um escândalo ambiental.",
+            "sinopse": "Baseado em fatos reais, o filme acompanha Robert Bilott, um advogado ambiental que descobre que uma das maiores corporações químicas do mundo está contaminando uma cidade com produtos tóxicos. À medida que ele se aprofunda no caso, coloca sua carreira, família e vida em risco para revelar a verdade.",
+            "genero": "Drama / Suspense",
+            "diretor": "Todd Haynes",
+            "destaque": false,
+            "data_lancamento": "2019-11-22",
+            "imagem_principal": "assets/assets/precoDaVerdade.webp",
+            "imagens_complementares": [
+                {
+                    "id": 1,
+                    "src": "assets/assets/preco_verdade01.webp"
+                },
+                {
+                    "id": 2,
+                    "src": "assets/assets/preco_verdade02.webp"
+                },
+                {
+                    "id": 3,
+                    "src": "assets/assets/preco_verdade03.webp"
+                },
+                {
+                    "id": 4,
+                    "src": "assets/assets/preco_verdade04.webp"
+                }
+            ]
+        },
+        {
+            "id": 3,
+            "titulo": "Um Contratempo",
+            "descricao": "Um empresário é acusado de assassinato e contrata uma especialista para construir sua defesa.",
+            "sinopse": "Adrián Doria é acusado de matar sua amante em um quarto de hotel trancado por dentro. Para provar sua inocência, ele contrata Virginia Goodman, uma especialista em preparar testemunhas. Em uma noite de interrogatório intenso, segredos ocultos vêm à tona.",
+            "genero": "Suspense / Crime",
+            "diretor": "Oriol Paulo",
+            "destaque": true,
+            "data_lancamento": "2017-01-06",
+            "imagem_principal": "assets/assets/umContratempo.webp",
+            "imagens_complementares": [
+                {
+                    "id": 1,
+                    "src": "assets/assets/contratempo01.webp",
+                },
+                {
+                    "id": 2,
+                    "src": "assets/assets/contratempo02.webp",
+                },
+                {
+                    "id": 3,
+                    "src": "assets/assets/contratempo03.webp",
+                },
+                {
+                    "id": 4,
+                    "src": "assets/assets/contratempo04.webp",
+                },
+            ]
+        },
+    ],
+    "filmesCards": [
+        {
+            "id": 4,
+            "titulo": "Garota Exemplar",
+            "descricao": "O desaparecimento de uma mulher revela segredos sombrios de um casamento perfeito.",
+            "sinopse": "Nick Dunne se torna o principal suspeito do desaparecimento de sua esposa, Amy. À medida que a investigação avança, segredos perturbadores são revelados sobre o casal.",
+            "genero": "Suspense / Drama",
+            "diretor": "David Fincher",
+            "destaque": true,
+            "data_lancamento": "2014-10-03",
+            "imagem_principal": "assets/assets/goneGirl.webp",
+            "imagens_complementares": [
+                { "id": 1, "src": "assets/assets/gone_girl01.webp" },
+                { "id": 2, "src": "assets/assets/gone_girl02.webp" },
+                { "id": 3, "src": "assets/assets/gone_girl03.webp" },
+                { "id": 4, "src": "assets/assets/gone_girl04.webp" },
+            ]
+        },
+        {
+            "id": 5,
+            "titulo": "Os Suspeitos",
+            "descricao": "Um pai desesperado vai até as últimas consequências para encontrar sua filha desaparecida.",
+            "sinopse": "Quando sua filha desaparece, Keller Dover suspeita de um jovem estranho. Sem provas, ele decide agir por conta própria, enquanto o detetive responsável tenta resolver o caso.",
+            "genero": "Suspense / Crime",
+            "diretor": "Denis Villeneuve",
+            "destaque": false,
+            "data_lancamento": "2013-09-20",
+            "imagem_principal": "assets/assets/osSuspeitos.webp",
+            "imagens_complementares": [
+                { "id": 1, "src": "assets/assets/suspeitos01.webp", },
+                { "id": 2, "src": "assets/assets/suspeitos02.webp", },
+                { "id": 3, "src": "assets/assets/suspeitos03.webp", },
+                { "id": 4, "src": "assets/assets/suspeitos04.webp", },
+            ]
+        },
+        {
+            "id": 6,
+            "titulo": "O Segredo dos Seus Olhos",
+            "descricao": "Um ex-investigador tenta resolver um caso antigo que o persegue há décadas.",
+            "sinopse": "Benjamín Esposito decide escrever um romance baseado em um caso não resolvido do passado. Sua investigação revela traumas pessoais e uma verdade inesperada.",
+            "genero": "Suspense / Drama",
+            "diretor": "Juan José Campanella",
+            "destaque": true,
+            "data_lancamento": "2009-08-13",
+            "imagem_principal": "assets/assets/osSegredosDosSeusOlhos.webp",
+            "imagens_complementares": [
+                { "id": 1, "src": "assets/assets/segredoolhos01.webp", },
+                { "id": 2, "src": "assets/assets/segredoolhos02.webp", },
+                { "id": 3, "src": "assets/assets/segredoolhos03.webp", },
+                { "id": 4, "src": "assets/assets/segredoolhos04.webp", },
+            ]
+        },
+        {
+            "id": 7,
+            "titulo": "O Homem Duplicado",
+            "descricao": "Um professor descobre um ator idêntico a ele e mergulha em um jogo psicológico perturbador.",
+            "sinopse": "Adam Bell vive uma rotina monótona até encontrar um homem idêntico a ele. Obcecado, Adam inicia uma busca que revela conexões inexplicáveis.",
+            "genero": "Suspense / Psicológico",
+            "diretor": "Denis Villeneuve",
+            "destaque": false,
+            "data_lancamento": "2014-03-14",
+            "imagem_principal": "assets/assets/homemDuplicado.webp",
+            "imagens_complementares": [
+                { "id": 1, "src": "assets/assets/homeDuplicado01.webp", },
+                { "id": 2, "src": "assets/assets/homeDuplicado02.webp", },
+                { "id": 3, "src": "assets/assets/homeDuplicado03.webp", },
+                { "id": 4, "src": "assets/assets/homeDuplicado04.webp", },
+            ]
+        },
+        {
+            "id": 8,
+            "titulo": "O Labirinto do Fauno",
+            "descricao": "Uma garota encontra um mundo mágico em meio à guerra civil espanhola.",
+            "sinopse": "Ofelia, filha de um oficial franquista, encontra um labirinto mágico onde um fauno lhe propõe três tarefas para provar que é a reencarnação de uma princesa.",
+            "genero": "Fantasia / Drama / Suspense",
+            "diretor": "Guillermo del Toro",
+            "destaque": false,
+            "data_lancamento": "2006-10-11",
+            "imagem_principal": "assets/assets/oLabirinto.webp",
+            "imagens_complementares": [
+                { "id": 1, "src": "assets/assets/labirinto01.webp", },
+                { "id": 2, "src": "assets/assets/labirinto02.webp", },
+                { "id": 3, "src": "assets/assets/labirinto03.webp", },
+                { "id": 4, "src": "assets/assets/labirinto04.webp", },
+            ]
+        },
+        {
+            "id": 9,
+            "titulo": "Os Outros",
+            "descricao": "Uma mulher vive com os filhos em uma mansão assombrada.",
+            "sinopse": "Durante a Segunda Guerra, Grace vive em uma casa isolada com os filhos, que têm uma condição rara de fotossensibilidade. Coisas estranhas começam a acontecer e segredos sobre a casa vêm à tona.",
+            "genero": "Terror / Suspense",
+            "diretor": "Alejandro Amenábar",
+            "destaque": true,
+            "data_lancamento": "2001-08-02",
+            "imagem_principal": "assets/assets/theOthers.webp",
+            "imagens_complementares": [
+                { "id": 1, "src": "assets/assets/outros01.webp", },
+                { "id": 2, "src": "assets/assets/outros02.webp", },
+                { "id": 3, "src": "assets/assets/outros03.webp", },
+                { "id": 4, "src": "assets/assets/outros04.webp", },
+            ]
+        },
+        {
+            "id": 10,
+            "titulo": "Antes de Dormir",
+            "descricao": "Uma mulher perde a memória toda vez que dorme.",
+            "sinopse": "Christine acorda todos os dias sem lembrar do passado. Com a ajuda de um médico, ela começa a gravar vídeos e descobrir a verdade sobre o que aconteceu com ela.",
+            "genero": "Suspense / Drama",
+            "diretor": "Rowan Joffé",
+            "destaque": false,
+            "data_lancamento": "2014-09-05",
+            "imagem_principal": "assets/assets/antesDormir.webp",
+            "imagens_complementares": [
+                { "id": 1, "src": "assets/assets/antes01.webp", },
+                { "id": 2, "src": "assets/assets/antes02.webp", },
+                { "id": 3, "src": "assets/assets/antes03.webp", },
+                { "id": 4, "src": "assets/assets/antes04.webp", },
+            ]
+        },
+        {
+            "id": 11,
+            "titulo": "Amnésia",
+            "descricao": "Um homem com perda de memória de curto prazo tenta encontrar o assassino da esposa.",
+            "sinopse": "Leonard tem um tipo raro de amnésia e usa tatuagens e fotos para registrar pistas. Mas conforme avança, a verdade se torna cada vez mais distorcida.",
+            "genero": "Mistério / Suspense",
+            "diretor": "Christopher Nolan",
+            "destaque": true,
+            "data_lancamento": "2000-10-11",
+            "imagem_principal": "assets/assets/memento.webp",
+            "imagens_complementares": [
+                { "id": 1, "src": "assets/assets/memento01.webp", },
+                { "id": 2, "src": "assets/assets/memento02.webp", },
+                { "id": 3, "src": "assets/assets/memento03.webp", },
+                { "id": 4, "src": "assets/assets/memento04.webp", },
+            ]
+        },
+        {
+            "id": 12,
+            "titulo": "Cisne Negro",
+            "descricao": "A busca pela perfeição leva uma bailarina a uma espiral de loucura.",
+            "sinopse": "Nina é escolhida como protagonista do balé 'O Lago dos Cisnes'. A pressão do papel e a chegada de uma rival abalam sua sanidade.",
+            "genero": "Psicológico / Drama / Suspense",
+            "diretor": "Darren Aronofsky",
+            "destaque": false,
+            "data_lancamento": "2010-12-03",
+            "imagem_principal": "assets/assets/cisneNegro.webp",
+            "imagens_complementares": [
+                { "id": 1, "src": "assets/assets/cisne01.webp", },
+                { "id": 2, "src": "assets/assets/cisne02.webp", },
+                { "id": 3, "src": "assets/assets/cisne03.webp", },
+                { "id": 4, "src": "assets/assets/cisne04.webp", },
+            ]
+        },
+        {
+            "id": 13,
+            "titulo": "O Orfanato",
+            "descricao": "Uma mulher retorna ao orfanato onde cresceu e coisas estranhas começam a acontecer.",
+            "sinopse": "Laura quer reabrir o orfanato da infância como lar para crianças com deficiência. Quando seu filho desaparece, ela descobre segredos obscuros do lugar.",
+            "genero": "Terror / Suspense",
+            "diretor": "J.A. Bayona",
+            "destaque": false,
+            "data_lancamento": "2007-10-11",
+            "imagem_principal": "assets/assets/orfanato.webp",
+            "imagens_complementares": [
+                { "id": 1, "src": "assets/assets/orfanato01.webp", },
+                { "id": 2, "src": "assets/assets/orfanato02.webp", },
+                { "id": 3, "src": "assets/assets/orfanato03.webp", },
+                { "id": 4, "src": "assets/assets/orfanato04.webp", },
+            ]
+        },
+        {
+            "id": 14,
+            "titulo": "Fragmentado",
+            "descricao": "Um homem com 23 personalidades diferentes sequestra três garotas.",
+            "sinopse": "Kevin, diagnosticado com transtorno dissociativo de identidade, sequestra adolescentes para um ritual. Suas múltiplas personalidades entram em conflito, e uma delas pode ser letal.",
+            "genero": "Suspense / Psicológico",
+            "diretor": "M. Night Shyamalan",
+            "destaque": true,
+            "data_lancamento": "2017-01-20",
+            "imagem_principal": "assets/assets/fragmentado.webp",
+            "imagens_complementares": [
+                { "id": 1, "src": "assets/assets/fragmentado01.webp", },
+                { "id": 2, "src": "assets/assets/fragmentado02.webp", },
+                { "id": 3, "src": "assets/assets/fragmentado03.webp", },
+                { "id": 4, "src": "assets/assets/fragmentado04.webp", },
+            ]
+        },
+        {
+            "id": 15,
+            "titulo": "O Código Da Vinci",
+            "descricao": "Um simbologista e uma criptóloga investigam um assassinato no Louvre.",
+            "sinopse": "O professor Robert Langdon se envolve em uma corrida para decifrar pistas escondidas em obras de arte. A busca o leva a uma verdade que pode abalar os alicerces da Igreja.",
+            "genero": "Mistério / Thriller",
+            "diretor": "Ron Howard",
+            "destaque": false,
+            "data_lancamento": "2006-05-19",
+            "imagem_principal": "assets/assets/codigoDaVinci.webp",
+            "imagens_complementares": [
+                { "id": 1, "src": "assets/assets/codigo01.webp", },
+                { "id": 2, "src": "assets/assets/codigo02.webp", },
+                { "id": 3, "src": "assets/assets/codigo03.webp", },
+                { "id": 4, "src": "assets/assets/codigo04.webp", },
+            ]
+        }
+    ]
+}
